e78786b399d3ca77c1357ace7fbbf07a6a594d53
Viacheslav Teverovskii <viacheslav.teverovskii@gmail.com>
2024-09-25 17:53:38 +0300

    Removed code duplication

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
-        System.out.println("Добро пожаловать в \"Калькулятор-cli\"\n");
-        System.out.println("Будьте внимательны - программа работает только с натуральными числами");
-        System.out.println("Допустимые операторы для использования: +, -, *, /, ^, %\n");
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Введите первое число: ");
-        int firstNum;
+    public static int setNumber(Scanner sc) {
+        int number;
         while (true) {
             if (sc.hasNextInt()) {
-                firstNum = sc.nextInt();
-                if (firstNum > 0) break;
+                number = sc.nextInt();
+                if (number > 0) break;
             }
             System.out.print("Неправильный формат! Попробуйте снова: ");
             sc.nextLine();
         }
+        return number;
+    }
 
-        System.out.print("Введите оператор: ");
+    public static String setOperator(Scanner sc) {
         String operator;
         while (true) {
             operator = sc.next();
@@ -34,36 +29,43 @@ public class Calculator {
             System.out.print("Неправильный оператор! Попробуйте снова: ");
             sc.nextLine();
         }
+        return operator;
+    }
 
-        System.out.print("Введите второе число: ");
-        int secondNum;
-        while (true) {
-            if (sc.hasNextInt()) {
-                secondNum = sc.nextInt();
-                if (secondNum > 0) break;
-            }
-            System.out.print("Неправильный формат! Попробуйте снова: ");
-            sc.nextLine();
-        }
-
-        int result = firstNum;
+    public static int getResult(int firstNumber, String operator, int secondNumber) {
+        int result = firstNumber;
         if (operator.equals("+")) {
-            result += secondNum;
+            result += secondNumber;
         } else if (operator.equals("-")) {
-            result -= secondNum;
+            result -= secondNumber;
         } else if (operator.equals("*")) {
-            result *= secondNum;
+            result *= secondNumber;
         } else if (operator.equals("/")) {
-            result /= secondNum;
+            result /= secondNumber;
         } else if (operator.equals("%")) {
-            result %= secondNum;
+            result %= secondNumber;
         } else {
-            for (int i = 1; i < secondNum; i++) {
-                result *= firstNum;
+            for (int i = 1; i < secondNumber; i++) {
+                result *= firstNumber;
             }
         }
+        return result;
+    }
 
-        System.out.printf("%n%d %s %d = %d", firstNum, operator, secondNum, result);
+    public static void main(String[] args) {
+        System.out.println("Добро пожаловать в \"Калькулятор-cli\"\n");
+        System.out.println("Будьте внимательны - программа работает только с натуральными числами");
+        System.out.println("Допустимые операторы для использования: +, -, *, /, ^, %\n");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int firstNumber = setNumber(sc);
+        System.out.print("Введите оператор: ");
+        String operator = setOperator(sc);
+        System.out.print("Введите второе число: ");
+        int secondNumber = setNumber(sc);
+        int result = getResult(firstNumber, operator, secondNumber);
+        System.out.printf("%n%d %s %d = %d", firstNumber, operator, secondNumber, result);
         sc.close();
     }
 }
