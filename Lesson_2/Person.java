a5acaa3eec406756365e455688ded9be0c5112f4
Viacheslav Teverovskii <viacheslav.teverovskii@gmail.com>
2024-09-26 20:47:44 +0300

    Implemented Person class

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String gender = "Мужской";
+    String name = "Вячеслав";
+    float height = 178.2f;
+    float weight = 72.5f;
+    short age = 31;
+
+    public void move() {
+        System.out.println(name + "идёт");
+    }
+
+    public void seat() {
+        System.out.println(name + "сидит");
+    }
+
+    public void run() {
+        System.out.println(name + "бежит");
+    }
+
+    public void speak() {
+        System.out.println(name + "говорит");
+    }
+
+    public void learnJava() {
+        System.out.println(name + "учит Java");
+    }
+}
