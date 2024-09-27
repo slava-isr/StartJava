cac3c5217b56c2ffeffe27532c9a201e5a5bc343
Viacheslav Teverovskii <viacheslav.teverovskii@gmail.com>
2024-09-27 18:42:53 +0300

    Implemented getters and setters

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,10 +1,54 @@
 public class Wolf {
 
-    String gender;
-    String name;
-    String color;
-    float weight;
-    short age;
+    private String gender;
+    private String name;
+    private String color;
+    private float weight;
+    private int age;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
 
     public void move() {
         System.out.println("Идёт");
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,15 +1,16 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.gender = "Кабель";
-        wolf.name = "Ява";
-        wolf.color = "Серый";
-        wolf.weight = 39.5f;
-        wolf.age = 9;
+        wolf.setGender("Кабель");
+        wolf.setName("Ява");
+        wolf.setColor("Серый");
+        wolf.setWeight(39.5f);
+        wolf.setAge(9);
 
-        System.out.println("Перед нами волк по кличке " + wolf.name + ", это " + wolf.gender);
-        System.out.println("Посмотрите на его красивый " + wolf.color + " цвет");
-        System.out.println("Это молодой и грозный " + wolf.age + "-летний волк весом " + wolf.weight + " килограмм");
+        System.out.println("Перед нами волк по кличке " + wolf.getName() + ", это " + wolf.getGender());
+        System.out.println("Посмотрите на его красивый " + wolf.getColor() + " цвет");
+        System.out.println("Это молодой и грозный " + wolf.getAge() + "-летний волк");
+        System.out.println("Весит он " + wolf.getWeight() + " килограмм");
         System.out.println("\nЧто же делает сейчас этот волк? А он сейчас:\n");
 
         wolf.move();
