b238bf9068198a60dbe1f39b771984085362feac
Viacheslav Teverovskii <viacheslav.teverovskii@gmail.com>
2024-10-18 22:23:33 +0300

    Correction after checking by teacher

--- src/com/startjava/lesson_2_3_4/array/ReverseNumbers.java
+++ src/com/startjava/lesson_2_3_4/array/ReverseNumbers.java
@@ -5,21 +5,21 @@ import java.util.Arrays;
 public class ReverseNumbers {
 
     public static void main(String[] args) {
-        int[] numbers = new int[]{};
-        int[] reversedNumbers = reverse(numbers);
-        displayResult(numbers, reversedNumbers);
-        numbers = null;
-        reversedNumbers = reverse(numbers);
-        displayResult(numbers, reversedNumbers);
-        numbers = new int[]{6, 8, 9, 1};
-        reversedNumbers = reverse(numbers);
-        displayResult(numbers, reversedNumbers);
-        numbers = new int[]{13, 8, 5, 3, 2, 1, 1};
-        reversedNumbers = reverse(numbers);
-        displayResult(numbers, reversedNumbers);
+        int[] sourceNumbers = new int[]{};
+        int[] reversedNumbers = toReverse(sourceNumbers);
+        displayResult(sourceNumbers, reversedNumbers);
+        sourceNumbers = null;
+        reversedNumbers = toReverse(sourceNumbers);
+        displayResult(sourceNumbers, reversedNumbers);
+        sourceNumbers = new int[]{6, 8, 9, 1};
+        reversedNumbers = toReverse(sourceNumbers);
+        displayResult(sourceNumbers, reversedNumbers);
+        sourceNumbers = new int[]{13, 8, 5, 3, 2, 1, 1};
+        reversedNumbers = toReverse(sourceNumbers);
+        displayResult(sourceNumbers, reversedNumbers);
     }
 
-    private static int[] reverse(int[] numbers) {
+    private static int[] toReverse(int[] numbers) {
         if (numbers == null || numbers.length == 0) return numbers;
         int len = numbers.length;
         int[] reversedNumbers = new int[len];
@@ -29,11 +29,11 @@ public class ReverseNumbers {
         return reversedNumbers;
     }
 
-    private static void displayResult(int[] numbers, int[] reversedNumbers) {
-        if (numbers == null || numbers.length == 0) {
-            System.out.printf("Ошибка: передан недопустимый массив (%s)%n%n", Arrays.toString(numbers));
+    private static void displayResult(int[] sourceNumbers, int[] reversedNumbers) {
+        if (sourceNumbers == null || sourceNumbers.length == 0) {
+            System.out.printf("Ошибка: передан недопустимый массив (%s)%n%n", Arrays.toString(sourceNumbers));
         } else {
-            System.out.printf("   До реверса: %s%n", Arrays.toString(numbers));
+            System.out.printf("   До реверса: %s%n", Arrays.toString(sourceNumbers));
             System.out.printf("После реверса: %s%n%n", Arrays.toString(reversedNumbers));
         }
     }
