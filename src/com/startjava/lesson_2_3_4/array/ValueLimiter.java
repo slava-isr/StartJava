c556cbf547d2f99c9c47968cdf34067672ddadfc
Viacheslav Teverovskii <viacheslav.teverovskii@gmail.com>
2024-10-19 21:01:04 +0300

    Correction after checking by teacher

--- src/com/startjava/lesson_2_3_4/array/ValueLimiter.java
+++ src/com/startjava/lesson_2_3_4/array/ValueLimiter.java
@@ -4,23 +4,23 @@ import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class ValueLimiter {
-    private static final int ARRAY_SIZE = 15;
     private static int resetCounter = 0;
 
     public static void main(String[] args) {
         int[] thresholdValueIndexes = {-1, 15, 0, 14};
         for (int index : thresholdValueIndexes) {
-            displayResult(index, resetValues(index));
+            double[] sourceValues = new double[15];
+            Arrays.setAll(sourceValues, value -> Math.random());
+            displayResult(index, sourceValues, resetValues(index, sourceValues));
         }
     }
 
-    private static double[][] resetValues(int thresholdValueIndex) {
-        if (thresholdValueIndex < 0 || thresholdValueIndex >= ARRAY_SIZE) return null;
-        double[] sourceValues = new double[ARRAY_SIZE];
-        Arrays.setAll(sourceValues, value -> Math.random());
-        double[] overwrittenValues = new double[ARRAY_SIZE];
+    private static double[] resetValues(int thresholdValueIndex, double[] sourceValues) {
+        int len = sourceValues.length;
+        if (thresholdValueIndex < 0 || thresholdValueIndex >= len) return null;
+        double[] overwrittenValues = new double[len];
         resetCounter = 0;
-        for (int i = 0; i < ARRAY_SIZE; i++) {
+        for (int i = 0; i < len; i++) {
             if (Double.compare(sourceValues[i], sourceValues[thresholdValueIndex]) > 0) {
                 overwrittenValues[i] = 0;
                 resetCounter++;
@@ -28,26 +28,25 @@ public class ValueLimiter {
                 overwrittenValues[i] = sourceValues[i];
             }
         }
-        return new double[][]{sourceValues, overwrittenValues};
+        return overwrittenValues;
     }
 
-    private static void displayResult(int thresholdValueIndex, double[][] results) {
-        if (thresholdValueIndex < 0 || thresholdValueIndex >= ARRAY_SIZE) {
+    private static void displayResult(int thresholdValueIndex, double[] sourceValues, double[] overwrittenValues) {
+        int len = sourceValues.length;
+        if (thresholdValueIndex < 0 || thresholdValueIndex >= len) {
             System.out.print("Ошибка: индекс должен быть в диапазоне [0, ");
-            System.out.printf("%d] (%d)%n%n", ARRAY_SIZE - 1, thresholdValueIndex);
+            System.out.printf("%d] (%d)%n%n", len - 1, thresholdValueIndex);
             return;
         }
         DecimalFormat df = new DecimalFormat("0.000");
-        double[] sourceValues = results[0];
         System.out.println("Исходный массив:");
         System.out.println(valuesToString(Arrays.copyOfRange(sourceValues, 0, 8), df));
-        System.out.println(valuesToString(Arrays.copyOfRange(sourceValues, 8, ARRAY_SIZE), df));
+        System.out.println(valuesToString(Arrays.copyOfRange(sourceValues, 8, len), df));
         System.out.printf("Значение по индексу (%d): ", thresholdValueIndex);
         System.out.printf("%s%n%n", df.format(sourceValues[thresholdValueIndex]));
-        double[] overwrittenValues = results[1];
         System.out.println("Измененный массив:");
         System.out.println(valuesToString(Arrays.copyOfRange(overwrittenValues, 0, 8), df));
-        System.out.println(valuesToString(Arrays.copyOfRange(overwrittenValues, 8, ARRAY_SIZE), df));
+        System.out.println(valuesToString(Arrays.copyOfRange(overwrittenValues, 8, len), df));
         System.out.printf("Количество обнуленных ячеек: %d%n%n", resetCounter);
     }
 
