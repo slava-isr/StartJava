d668ce5478f21aca7fe7825e2bde9ab4263cbc90
Viacheslav Teverovskii <viacheslav.teverovskii@gmail.com>
2024-11-13 21:17:40 +0200

    Correction after checking by teacher

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -4,37 +4,46 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculatorTest {
+    private static boolean isCalculated = false;
+    private static boolean isCorrectUserAnswer = true;
 
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в \"Калькулятор-cli\".");
         System.out.println("Будьте внимательны - программа работает только с целыми числами.");
         Calculator calculator = new Calculator();
         Scanner sc = new Scanner(System.in);
-        for (; true; ) {
-            System.out.print("\nВведите выражение из трех аргументов, например, 2 ^ 10: ");
-            String expression = sc.nextLine().trim().replaceAll("\\s+", " ");
-            double result = calculator.calculate(expression);
-            if (Double.isNaN(result) || Double.isInfinite(result)) {
-                System.out.println("\nУпс! Получено недопустимое значение. Проверьте выражение и попробуйте снова.");
-                continue;
+        do {
+            if (!isCalculated) {
+                System.out.print("\nВведите выражение из трех аргументов, например, 2 ^ 10: ");
+                String expression = sc.nextLine().trim().replaceAll("\\s+", " ");
+                printFormattedResult(expression, calculator.calculate(expression));
             }
-            printFormattedResult(expression, result);
-            if (!promptToContinue("\nХотите продолжить вычисления? [yes / no]: ", sc)) break;
-        }
+        } while (isContinueCalculate(sc));
         System.out.println("\nДо свидания!");
         sc.close();
     }
 
     private static void printFormattedResult(String expression, double result) {
-        DecimalFormat df = new DecimalFormat("#.####");
-        System.out.printf("%n%s = %s%n", expression, df.format(result));
+        if (Double.isNaN(result) || Double.isInfinite(result)) {
+            System.out.println("\nУпс! Получено недопустимое значение. Проверьте выражение и попробуйте снова.");
+        } else {
+            DecimalFormat df = new DecimalFormat("#.####");
+            System.out.printf("%n%s = %s%n", expression, df.format(result));
+        }
+        isCalculated = true;
     }
 
-    private static boolean promptToContinue(String message, Scanner sc) {
-        System.out.print(message);
-        String playerAnswer = sc.nextLine().trim().toLowerCase();
-        if ("yes".equals(playerAnswer)) return true;
-        if ("no".equals(playerAnswer)) return false;
-        return promptToContinue("\nВведите корректный ответ [yes / no]: ", sc);
+    private static boolean isContinueCalculate(Scanner sc) {
+        System.out.printf("%n%s [yes / no]: ",
+                isCorrectUserAnswer ? "Хотите продолжить вычисления?" : "Введите корректный ответ");
+        String userAnswer = sc.nextLine().trim().toLowerCase();
+        if ("no".equals(userAnswer)) return false;
+        if ("yes".equals(userAnswer)) {
+            isCalculated = false;
+            isCorrectUserAnswer = true;
+            return true;
+        }
+        isCorrectUserAnswer = false;
+        return true;
     }
 }
